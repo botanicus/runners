ad07d3a01cac6d115e183dba97b5cb413b1a9ba3
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-04-13 16:54:17 +0000

    X

--- instapaper/runner.rb
+++ instapaper/runner.rb
@@ -1,1 +1,16 @@
 #!/usr/bin/env ruby
+
+require 'instapaper'
+
+username = ENV.fetch('INSTAPAPER_USERNAME')
+password = ENV.fetch('INSTAPAPER_PASSWORD')
+
+client = Instapaper::Client.new
+client.access_token(username, password)
+
+client = Instapaper::Client.new do |client|
+  client.consumer_key = YOUR_CONSUMER_KEY
+  client.consumer_secret = YOUR_CONSUMER_SECRET
+  client.oauth_token = YOUR_OAUTH_TOKEN
+  client.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+end
