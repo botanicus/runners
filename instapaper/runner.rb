d5ba9bc8fdacc7a07040b9a11d77e0bc1c795c42
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-04-13 19:27:52 +0000

    Auth works

--- instapaper/runner.rb
+++ instapaper/runner.rb
@@ -2,15 +2,19 @@
 
 require 'instapaper'
 
+client_key = ENV.fetch('INSTAPAPER_OAUTH_CONSUMER_ID')
+client_secret = ENV.fetch('INSTAPAPER_OAUTH_CONSUMER_SECRET')
 username = ENV.fetch('INSTAPAPER_USERNAME')
 password = ENV.fetch('INSTAPAPER_PASSWORD')
 
-client = Instapaper::Client.new
-client.access_token(username, password)
-
 client = Instapaper::Client.new do |client|
-  client.consumer_key = YOUR_CONSUMER_KEY
-  client.consumer_secret = YOUR_CONSUMER_SECRET
-  client.oauth_token = YOUR_OAUTH_TOKEN
-  client.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+  client.consumer_key = client_key
+  client.consumer_secret = client_secret
+
+  token = client.access_token(username, password)
+  client.oauth_token = token.oauth_token
+  client.oauth_token_secret = token.oauth_token_secret
 end
+
+puts
+p client.bookmarks
