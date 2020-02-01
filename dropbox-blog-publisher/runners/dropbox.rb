05c988580eec630287b62ae0fb43d5992ae5deaf
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2020-02-01 14:49:30 +0000

    Refactor methods

--- dropbox-blog-publisher/runners/dropbox.rb
+++ dropbox-blog-publisher/runners/dropbox.rb
@@ -20,6 +20,34 @@ class DropboxRunner
     published_files_request.entries
   end
 
+  # User by runner.rb.
+  def publish_drop_folder_entries
+    drop_folder_items = @dropbox.list_folder(@runner.config.drop_folder)
+
+    if drop_folder_items.entries.empty?
+      @runner.info("Nothing found in #{@runner.config.drop_folder}")
+    else
+      main_post_file = drop_folder_items.entries.find { |file| file.name.match(/\.md$/) }
+      slug = main_post_file.name.split('.').first
+      @runner.notify(title: "Publishing #{slug}")
+      @runner.info("Publishing #{slug}")
+
+      publish_post(main_post_file)
+
+      drop_folder_items = @dropbox.list_folder(@runner.config.drop_folder)
+      timestamp = Time.now.strftime('%Y-%m-%d')
+      published_post_path = File.join(@runner.config.archive_folder, "#{timestamp}-#{slug}")
+
+      @runner.info("Publishing #{published_post_path}")
+      @dropbox.create_folder(published_post_path)
+      drop_folder_items.entries.each do |file|
+        @runner.info("Moving #{file.name} -> #{published_post_path}")
+        @dropbox.move(file.path_display, File.join(published_post_path, file.name))
+      end
+    end
+  end
+
+  protected
   def publish_post(main_post_file)
     @dropbox.download(main_post_file.path_display) do |content|
       content = content.force_encoding('utf-8')
@@ -51,33 +79,6 @@ class DropboxRunner
     @runner.notify(title: "Post #{slug} cannot be published", message: "#{error.class}: #{error.message}")
   end
 
-  def publish_drop_folder_entries
-    drop_folder_items = @dropbox.list_folder(@runner.config.drop_folder)
-
-    if drop_folder_items.entries.empty?
-      @runner.info("Nothing found in #{@runner.config.drop_folder}")
-    else
-      main_post_file = drop_folder_items.entries.find { |file| file.name.match(/\.md$/) }
-      slug = main_post_file.name.split('.').first
-      @runner.notify(title: "Publishing #{slug}")
-      @runner.info("Publishing #{slug}")
-
-      publish_post(main_post_file)
-
-      drop_folder_items = @dropbox.list_folder(@runner.config.drop_folder)
-      timestamp = Time.now.strftime('%Y-%m-%d')
-      published_post_path = File.join(@runner.config.archive_folder, "#{timestamp}-#{slug}")
-
-      @runner.info("Publishing #{published_post_path}")
-      @dropbox.create_folder(published_post_path)
-      drop_folder_items.entries.each do |file|
-        @runner.info("Moving #{file.name} -> #{published_post_path}")
-        @dropbox.move(file.path_display, File.join(published_post_path, file.name))
-      end
-    end
-  end
-
-  protected
   def guard(request)
     if request.has_more?
       @runner.notify(title: "Blog error", message: "Dropbox pagination error, there might be missing files.")
